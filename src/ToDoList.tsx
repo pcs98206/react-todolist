4bafa8f2bb0adfe7d36dda4206c17277b3789ecb
Sangkyu <i9704@naver.com>
2022-11-11 07:42:56 +0900

    2. react-hook-from setup

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-const ToDoList = () => {
+/* const ToDoList = () => {
   const [toDo, setTodo] = useState("");
+  const [toDoError, setToDoError] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
+    setToDoError("");
     setTodo(value);
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(toDo);
+    if (toDo.length < 10) {
+      return setToDoError("To do should be longer");
+    }
+    console.log(toDoError);
   };
   return (
     <div>
@@ -21,6 +27,24 @@ const ToDoList = () => {
           type="text"
           placeholder="Write a to do"
         />
+        <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
+      </form>
+    </div>
+  );
+}; */
+
+const ToDoList = () => {
+  const { register, watch } = useForm();
+  console.log(watch());
+  return (
+    <div>
+      <form action="">
+        <input {...register("toDO1")} type="text" placeholder="Write a to do" />
+        <input {...register("toDO2")} type="text" placeholder="Write a to do" />
+        <input {...register("toDO3")} type="text" placeholder="Write a to do" />
+        <input {...register("toDO4")} type="text" placeholder="Write a to do" />
+        <input {...register("toDO5")} type="text" placeholder="Write a to do" />
         <button>Add</button>
       </form>
     </div>
