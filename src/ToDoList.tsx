6d7a31abb679f5b7cbcfe45e7e2caa6e2b81783f
Sangkyu <i9704@naver.com>
2022-11-16 07:54:25 +0900

    Form validation and Errors

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -34,17 +34,76 @@ import { useForm } from "react-hook-form";
   );
 }; */
 
+interface IFormData {
+  email: string;
+  toDO2: string;
+  toDO3: string;
+  toDO4: string;
+  toDO5: string;
+}
+
 const ToDoList = () => {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormData>({
+    defaultValues: {
+      email: "naver.com",
+    },
+  });
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(errors);
   return (
     <div>
-      <form action="">
-        <input {...register("toDO1")} type="text" placeholder="Write a to do" />
-        <input {...register("toDO2")} type="text" placeholder="Write a to do" />
-        <input {...register("toDO3")} type="text" placeholder="Write a to do" />
-        <input {...register("toDO4")} type="text" placeholder="Write a to do" />
-        <input {...register("toDO5")} type="text" placeholder="Write a to do" />
+      <form
+        action=""
+        onSubmit={handleSubmit(onValid)}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <input
+          {...register("email", {
+            required: "email is required",
+            minLength: {
+              value: 5,
+              message: "email is very short",
+            },
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          type="text"
+          placeholder="email"
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("toDO2", { required: "write here" })}
+          type="text"
+          placeholder="Write a to do"
+        />
+        <span>{errors?.toDO2?.message}</span>
+        <input
+          {...register("toDO3", { required: "write here" })}
+          type="text"
+          placeholder="Write a to do"
+        />
+        <span>{errors?.toDO3?.message}</span>
+        <input
+          {...register("toDO4", { required: "write here" })}
+          type="text"
+          placeholder="Write a to do"
+        />
+        <span>{errors?.toDO4?.message}</span>
+        <input
+          {...register("toDO5", { required: "write here" })}
+          type="text"
+          placeholder="Write a to do"
+        />
+        <span>{errors?.toDO5?.message}</span>
         <button>Add</button>
       </form>
     </div>
